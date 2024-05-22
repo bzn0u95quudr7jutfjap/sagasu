1864f4a7d9cecfe3806e12c34b26847124ee2924
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-05-22 22:27:27 +0200

    init sagasu.php

---
+++ sagasu.php
@@ -0,0 +1,41 @@
+#!/bin/php
+<?php
+
+set_error_handler(function ($severity, $message, $file, $line) {
+  throw new \ErrorException($message, $severity, $severity, $file, $line);
+});
+
+set_exception_handler(function (Throwable $exception) {
+  $a = explode("\n", "{$exception->getMessage()}\n\n{$exception->getTraceAsString()}");
+  $format = '%-' . max(array_map('strlen', $a)) . 's' . "        <br>\n";
+  $a = array_map(fn ($a) => sprintf($format, $a), $a);
+  $a = implode("", $a);
+  die("\n" . $a . "\n");
+});
+
+function _find($directory) {
+  $depth = 12;
+  $glob = $directory . '/' . str_repeat('{,*/', $depth) . str_repeat('}', $depth) . '*';
+  $glob = array_filter(glob($glob, GLOB_BRACE), fn ($a) => !is_dir($a));
+  $glob = array_combine($glob, $glob);
+  return $glob;
+}
+
+function preg($r, $s) {
+  $a = [];
+  preg_match_all("/.*$r.*/", $s, $a);
+  return $a;
+}
+
+function findcontains($r, $g) {
+  return array_filter(
+    array_map(fn ($a) => preg($r, file_get_contents($a))[0], $g,),
+    fn ($a) => count($a),
+  );
+}
+
+$a = getopt('p:d:', ['pattern:', 'directory:',]);
+
+print_r(
+  findcontains($a['p'], _find(array_key_exists('d', $a) ? $a['d'] : '.'))
+);
