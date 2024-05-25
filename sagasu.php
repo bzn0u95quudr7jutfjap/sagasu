897c1ce10f3383e10c243a02cd60c6387b6aa23d
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-05-25 11:53:22 +0200

    rinomine

--- sagasu.php
+++ sagasu.php
@@ -31,7 +31,7 @@ function _reduce($f, $i) {
 
 // Corpo principale
 
-function _find($directory) {
+function find($directory) {
   $depth = fn ($f, $d, $a) => $d > 0 ? $f($f, $d - 1, "{,*/$a}") : $a;
   return _reduce(fn ($a, $f) => $f($a), $directory)([
     fn ($d) => "$d/{$depth($depth, 0, '')}/*",
@@ -41,7 +41,7 @@ function _find($directory) {
   ]);
 }
 
-function findcontains($r, $g) {
+function contains($r, $g) {
   return _reduce(fn ($a, $f) => $f($a), $g)([
     _map(fn ($a) => preg_grep("/.*$r.*/", explode("\n", file_get_contents($a))),),
     _filter(fn ($a) => count($a) > 0),
@@ -53,5 +53,5 @@ function findcontains($r, $g) {
 $a = getopt('p:d:', ['pattern:', 'directory:',]);
 
 print_r(
-  findcontains($a['p'], _find(array_key_exists('d', $a) ? $a['d'] : '.'))
+  contains($a['p'], find(array_key_exists('d', $a) ? $a['d'] : '.'))
 );
